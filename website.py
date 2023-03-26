4f52b64c1b13638bf7d90efc3f39461e213aea4a
SIDD2310 <siddharthbackup2310@gmail.com>
2023-03-26 18:44:11 +0400

    added plottly graphs

--- website.py
+++ website.py
@@ -4,6 +4,7 @@ import Scripts.record_speech
 import Scripts.audio_to_text
 import Scripts.summary
 import Scripts.text_to_speech
+import plotly.graph_objects as go
 
 import requests
 from PIL import Image
@@ -14,21 +15,23 @@ st.set_page_config(page_title="Clear Speak")
 
 st.write("<div style='text-align: center'><h1>Clear Speak</h1></div>", unsafe_allow_html=True)
 
-col1,col2 = st.columns(2)
+col1_main,col2_main = st.columns(2)
 
-with col1:
+with col1_main:
 
     input_text = st.text_area("Enter script for the speech",placeholder="Your speech.....")
     
     #st.write(summary.summary)
+    col1, col2 = st.columns(2)
+    
+    with col1:
+        st.button("Start recording")
+    with col2:
+        st.button("Stop recording")
 
-    st.button("Record/Stop")
+    st.audio("speech.wav")
 
-    st.audio('speech.wav')
-
-
-
-with col2:
+with col2_main:
 
     def load_lottieurl(url: str):
         r = requests.get(url)
@@ -43,5 +46,65 @@ with col2:
 
 
 st.write("<div style='text-align: center'><h2>Analytics</h2></div>", unsafe_allow_html=True)
+
+col1_anal, col2_anal = st.columns(2)
+
+with col1_anal:
+    st.write("Suggestions: ")
+    
+    
+with col2_anal:
+    
+
+    # fig = go.Figure(go.Indicator(
+    #     mode = "gauge+number",
+    #     value = 270,
+    #     domain = {'x': [0, 1], 'y': [0, 1]},
+    #     title = {'text': "Speed"}))
+    
+    import plotly.graph_objects as go
+
+    # fig = go.Figure(go.Indicator(
+    #     mode = "gauge+number+delta",
+    #     value = 90,
+    #     domain = {'x': [0, 1], 'y': [0, 1]},
+    #     title = {'text': "Speed", 'font': {'size': 24}},
+    #     delta = {'reference': 75, 'increasing': {'color': "RebeccaPurple"}},
+    #     gauge = {
+    #         'axis': {'range': [None, 100], 'tickwidth': 1, 'tickcolor': "darkblue"},
+    #         'bar': {'color': "darkblue"},
+            
+    #         'borderwidth': 2,
+    #         'bordercolor': "gray",
+    #         'steps': [
+    #             {'range': [75, 100], 'color': 'royalblue'}],
+    #         'threshold': {
+    #             'line': {'color': "red", 'width': 4},
+    #             'thickness': 0.75,
+    #             'value': 90}}))
+
+    # fig.update_layout(font = {'color': "darkblue", 'family': "Arial"})
+    
+    fig = go.Figure(go.Indicator(
+        mode = "gauge+number+delta",
+        value = 90,
+        domain = {'x': [0, 1], 'y': [0, 1]},
+        title = {'text': "Speed", 'font': {'size': 24}},
+        delta = {'reference': 75, 'increasing': {'color': "green"}},
+        gauge = {
+            'axis': {'range': [None, 100], 'tickwidth': 1, 'tickcolor': "royalblue"},
+            'bar': {'color': "royalblue"},
+            
+            'borderwidth': 2,
+            'bordercolor': "gray",
+            'steps': [],
+            'threshold': {
+                'line': {'color': "red", 'width': 4},
+                'thickness': 0.75,
+                'value': 90}}))
+
+    fig.update_layout(font = {'color': "white", 'family': "Arial"})
+
+    st.plotly_chart(fig, use_container_width=True)
 
 
